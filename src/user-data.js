2cc0ff73835ad95ec911fa0e9f899f4ad523ad9a
juansanchezjr2008 <2008juansanchezs@gmail.com>
2024-12-29 12:33:52 -0600

    Actualizacion de Articulos

---
+++ src/user-data.js
@@ -0,0 +1,10 @@
+// Datos de Registro de Usuario Local.
+const name = [];
+const password = [];
+const email = [];
+
+// Datos de Preferencias Locales
+
+const country = [];
+const registerChurch = [];
+const liricFolderBase = [];
